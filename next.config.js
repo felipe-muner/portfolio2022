7f35a21c692bb49030a7ff022bc3ff972f46cbc4
Felipe Muner <felipe.muner@gmail.com>
2022-08-31 13:24:36 -0300

    update webpack to work with top-level-await and type-graphql

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    // this will override the experiments
+    config.experiments = { ...config.experiments, ...{ topLevelAwait: true }};
+    // this will just update topLevelAwait property of config.experiments
+    // config.experiments.topLevelAwait = true 
+    console.log('config', config)
+    return config;
+  },
 }
 
 module.exports = nextConfig
